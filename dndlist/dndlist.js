ea0f2233a1ed4e7b2352d98652587adfc858837b
Muhammad Lukman Nasaruddin <anatilmizun@gmail.com>
2017-07-10 13:45:35 +0800

    [dndlist] Added support for using an {key:value} object as choices
    The keys of the object will be used as values for the dndlist while the values of the object will be used in the list display

--- dndlist/dndlist.js
+++ dndlist/dndlist.js
@@ -1,6 +1,6 @@
 /*
  * Webix Component "dndlist"
- * Version 1.0 
+ * Version 1.1 
  * By Muhammad Lukman Nasaruddin (https://github.com/MLukman/webix-components) 
  *
  * Requires Webix >= 3.1.1 
@@ -20,19 +20,19 @@ webix.protoUI({
 	$formElement: true,
 	$init: function (cfg) {
 		var dnd = this;
-		config = webix.extend(this.defaults, cfg, true);
+		this.config = webix.extend(this.defaults, cfg, true);
 		var dtCommon = {view: "datatable", scrollX: false, drag: true};
 		var value = [];
-		for (var i = 0; i < config.value.length; i++) {
-			value.push({value: config.value[i]});
+		for (var i = 0; i < this.config.value.length; i++) {
+			value.push({value: this.config.value[i]});
 		}
 		var choicesDt = webix.extend(webix.copy(dtCommon), {
 			id: 'choices',
 			columns: [
 				{
-					id: "value",
+					id: "display",
 					header: {
-						text: config.choicesHeader,
+						text: this.config.choicesHeader,
 						colspan: 2
 					},
 					fillspace: true
@@ -62,9 +62,9 @@ webix.protoUI({
 			id: 'value',
 			columns: [
 				{
-					id: "value",
+					id: "display",
 					header: {
-						text: config.valueHeader,
+						text: this.config.valueHeader,
 						colspan: 2
 					},
 					fillspace: true
@@ -91,19 +91,31 @@ webix.protoUI({
 			}
 		}, true);
 
-		var spacer = {view: 'spacer', width: 10};
-		var cols = {cols: (config.swap ? [valueDt, spacer, choicesDt] : [choicesDt, spacer, valueDt])};
-		var label = (config.label != "" ?
-				{id: 'label', view: 'label', label: config.label, width: config.labelWidth, align: config.labelAlign} :
+		var spacer = {view: 'resizer', borderless: true};
+		var cols = {cols: (this.config.swap ? [valueDt, spacer, choicesDt] : [choicesDt, spacer, valueDt])};
+		var top = (this.config.labelPosition == 'top');
+		var label = (this.config.label != "" ?
+				//{id: 'label', view: 'label', label: config.label, width: config.labelWidth, align: config.labelAlign} :
+				{
+					id: 'label',
+					view: 'template',
+					type: 'clean',
+					width: this.config.labelWidth,
+					css: { 'text-align' : this.config.labelAlign },
+					template: '<label style="display: inline-block; float: ' + this.config.labelAlign 
+					+ '" class="webix_inp_' + (top ? "top_":"") + 'label '
+					+ (this.config.required ? "webix_required":"")
+					+ '">' + this.config.label + '</label>'
+				}:
 				{width: 1, height: 1});
-		if (config.labelPosition === 'top') {
+		if (top) {
 			config.rows = [
 				label,
 				cols
 			];
 		} else {
-			label.css = {padding: "1px 7.5px 0px 0px"};
-			config.cols = [
+			//label.css = { 'padding-right': '7.5px' };
+			this.config.cols = [
 				{
 					rows: [label]
 				},
@@ -112,19 +124,32 @@ webix.protoUI({
 		}
 
 		this.$ready.push(function () {
-			this.setValue(config.value);
-			this.setChoices(config.choices);
+			this.setValue(this.config.value);
+			this.setChoices(this.config.choices);
+			$$(this.$view).$$('label').getNode().children[0].style.padding = 0;
+			//webix.html.addCss($$(this.$view).$$('label').getNode(), 'webix_inp_label');
+			//webix.html.removeCss($$(this.$view).$$('label').getNode(), 'webix_el_label');
 		});
 	},
 	setChoices: function (choices) {
-		var choicesDt = this.$$('choices');
+		var choicesDt = $$(this.$view).$$('choices');
 		choicesDt.clearAll();
 		var values = this.getValue();
-		for (var i = 0; i < choices.length; i++) {
-			if (values.indexOf(choices[i]) === -1) {
-				choicesDt.add({value: choices[i]});
+		if (Array.isArray(choices)) {
+			for (var i = 0; i < choices.length; i++) {
+				if (values.indexOf(choices[i]) === -1) {
+					choicesDt.add({value: choices[i], display: choices[i]});
+				}
 			}
 		}
+		else {
+			for (var value in choices) {
+				if (values.indexOf(value) === -1) {
+					choicesDt.add({value: value, display: choices[value]});
+				}
+			}
+		}
+		this.config.choices = choices;
 	},
 	setValue: function (values) {
 		var existVal = this.getValue();
@@ -139,7 +164,7 @@ webix.protoUI({
 	},
 	getValue: function () {
 		var value = [];
-		var valueDt = this.$$('value');
+		var valueDt = $$(this.$view).$$('value');
 		valueDt.eachRow(function (row) {
 			value.push(valueDt.getItem(row).value);
 		}, true);
@@ -148,11 +173,17 @@ webix.protoUI({
 	addValue: function (value) {
 		var valueDt = this.$$('value');
 		var choicesDt = this.$$('choices');
+		var choices = this.config.choices;
 		var rowFinder = (function (row) {
 			return row.value === value;
 		});
 		if (valueDt.find(rowFinder, true) == false) {
-			valueDt.add({value: value});
+			if (Array.isArray(choices)) {
+				valueDt.add({value: value, display: value});
+			}
+			else {
+				valueDt.add({value: value, display: choices[value]});
+			}
 		}
 		var choices = choicesDt.find(rowFinder);
 		for (var i = 0; i < choices.length; i++) {
@@ -162,18 +193,21 @@ webix.protoUI({
 	removeValue: function (val) {
 		var valueDt = this.$$('value');
 		var choicesDt = this.$$('choices');
+		var choices = this.config.choices;
 		var rowFinder = (function (row) {
 			return row.value === val;
 		});
 		var value = valueDt.find(rowFinder);
-		if (value.length === 0) {
-			return;
-		}
 		for (var i = 0; i < value.length; i++) {
 			valueDt.remove(value[i].id);
 		}
 		if (choicesDt.find(rowFinder, true) == false) {
-			choicesDt.add({value: val});
+			if (Array.isArray(choices)) {
+				choicesDt.add({value: val, display: val});
+				}
+			else if (choices.hasOwnProperty(val)) {
+				choicesDt.add({value: val, display: choices[val]});
+			}
 		}
 	}
 }, webix.IdSpace, webix.ui.layout);
